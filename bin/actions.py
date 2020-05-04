bd0d9df497f87f1a8c50af38cdb750fa1b33fb5f
keksnicoh <keksnicoh@googlemail.com>
2020-05-04 15:46:47 +0200

    added worker queue

--- bin/actions.py
+++ bin/actions.py
@@ -8,11 +8,12 @@ import getopt
 import sys
 from threading import Thread
 from rpi_rf import RFDevice
-
+import queue
 logger = logging.getLogger('websockets.server')
 logger.setLevel(logging.ERROR)
 logger.addHandler(logging.StreamHandler())
 
+q = queue.Queue()
 
 async def client(api):
 
@@ -24,11 +25,12 @@ async def client(api):
                     greeting = await websocket.recv()
                     action = json.loads(greeting)
                     if action['action'] in ['light', 'light_changed']:
+                        q.put(action['data'])
                         print("[{}] light".format(action['action']))
-                        Thread(
-                            target=action_light,
-                            args=(action['data'], )
-                        ).start()
+                        #Thread(
+                        #    target=action_light,
+                        #    args=(action['data'], )
+                        #).start()
                     elif action['action'] == 'ping':
                         print('got pinged...')
                         await websocket.send("pong")
@@ -43,22 +45,26 @@ async def client(api):
 
 
 def action_light(payload):
-
-    for i in range(0, 5):
-        if payload.get('light1', None) is not None:
-            switch(279889 if payload['light1'] else 279892)
-        if payload.get('light2', None) is not None:
-            switch(282961 if payload['light2'] else 282964)
-
-
-def switch(code):
-    print("code: {}".format(code))
     rfdevice = RFDevice(17)
     rfdevice.enable_tx()
     rfdevice.tx_repeat = 5
-    rfdevice.tx_code(code, 1, 302, 24)
-    rfdevice.close()
+    for i in range(0, 1):
+        if payload.get('light1', None) is not None:
+            switch(rfdevice, 279889 if payload['light1'] else 279892)
+        if payload.get('light2', None) is not None:
+            switch(rfdevice, 282961 if payload['light2'] else 282964)
+    rfdevice.cleanup()
 
+def switch(rfdevice, code):
+    print("code: {}".format(code))
+    rfdevice.tx_code(code, 1, 302, 24)
+
+
+def worker():
+    while True:
+        item = q.get()
+        print(item)
+        action_light(item)
 
 def main():
     api = "ws://localhost:8081/v1/stream"
@@ -70,6 +76,7 @@ def main():
         else:
             assert False, "unhandled option"
 
+    Thread(target=worker, daemon=True).start()
     asyncio.get_event_loop().run_until_complete(client(api))
 
 
